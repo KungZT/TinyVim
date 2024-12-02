4acb32c151ce37779442c3b5e806522120553616
kang <ongkzt@gmail.com>
2024-12-02 09:42:45 +0800

    波浪线绘制

--- screen.c
+++ screen.c
@@ -36,50 +36,56 @@ void editorProcessKeypress()
 
 void editorRefreshScreen()
 {
-    write(STDOUT_FILENO, "\x1b[2J", 4);//from unistd,,esc=27
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    struct abuf ab = ABUF_INIT;
+    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[H", 3);
+    editorDrawRows(&ab);
+    abAppend(&ab, "\x1b[H", 3);
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
+//     write(STDOUT_FILENO, "\x1b[2J", 4);//from unistd,,esc=27
+//     write(STDOUT_FILENO, "\x1b[H", 3);
 
-    editorDrawRows();
-    write(STDOUT_FILENO, "\x1b[H", 3);
-}
+//     editorDrawRows();
+//     write(STDOUT_FILENO, "\x1b[H", 3);
+// }
 
-void editorDrawRows()
+void editorDrawRows(struct abuf *ab)
 {
     int y;
     for (y = 0; y < E.screenrows; y++)
     {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        write(STDOUT_FILENO, "~", 1);
+        if (y < E.screenrows - 1){
+            write(STDOUT_FILENO, "\r\n", 2);}
     }
 }
 
 int getCursorPosition(int *rows, int *cols)
 {
-    // char buf[32];
-    // unsigned int i = 0;
+    char buf[32];
+    unsigned int i = 0;
     if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
 
-    printf("\r\n");
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1)
-    {
-        if (iscntrl(c)){
-            printf("%d\r\n",c);
-        }else{
-        printf("%d ('%c')\r\n",c,c);
-        }
+    while (i < sizeof(buf) - 1){
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
     }
-    editorReadKey();
+    buf[i] = '\0';
+    
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
 
-    return -1;
+    return 0;
 }
     int getWindowSize(int *rows, int *cols)
 {
     struct winsize ws;
-    if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
     {
         if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
-        editorReadKey();
-        return -1;
+        return getCursorPosition(rows, cols);
     }
     else
     {
@@ -93,4 +99,19 @@ void initEditor()
 {
     if (getWindowSize(&E.screenrows, &E.screencols) == -1)
         die("getWindowSize");
+}
+
+
+void abAppend(struct abuf *ab, const char *s, int len)
+{
+    char *new = realloc(ab->b, ab->len + len);
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab)
+{
+    free(ab->b);
 }
--- screen.h
+++ screen.h
@@ -1,17 +1,27 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
-
+#include <string.h>
+#include <stdio.h> 
 #include "RawMode.h"
 #include "TinyVim.h"
 
+struct abuf {
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
 
 char editorReadKey();
 void editorProcessKeypress();
 void editorRefreshScreen();
-void editorDrawRows();
+void editorDrawRows(struct abuf *ab);
 int getCursorPosition(int *rows, int *cols);
 int getWindowSize(int *rows, int *cols);
 void initEditor();
+void abAppend(struct abuf *ab, const char *s, int len);
+void abFree(struct abuf *ab);
+
 
 #endif
